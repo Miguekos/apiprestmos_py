36884e91010e25e184a517970539c0c711eebd87
Miguel Rodriguez <miguekos1233@gmail.com>
2020-05-01 23:46:41 -0500

    v0.2 conect mongo new server

--- mongo.py
+++ mongo.py
@@ -2,6 +2,7 @@ from app import app
 from flask_pymongo import PyMongo
 
 app.secret_key = "secret key"
-app.config["MONGO_URI"] = "mongodb://144.217.14.34:27017/prestamos"
+# app.config["MONGO_URI"] = "mongodb://144.217.14.34:27017/prestamos"
+app.config["MONGO_URI"] = "mongodb://95.111.235.214:32768/prestamos"
 # app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/prestamos"
 mongo = PyMongo(app)
